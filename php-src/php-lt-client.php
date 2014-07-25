2c1824a378407c3d16d70f1b58d42a3446423f30
Claes Gyllensvärd <letharion@gmail.com>
2014-07-25 23:12:53 +0100

    Still initial commit ;)

--- php-src/php-lt-client.php
+++ php-src/php-lt-client.php
@@ -2,51 +2,108 @@
 
 require_once 'vendor/autoload.php';
 
-$loop = React\EventLoop\Factory::create();
+use Aura\Cli\AbstractCommand;
+use Aura\Cli\Context;
+use Aura\Cli\ExceptionFactory;
+use Aura\Cli\Getopt;
+use Aura\Cli\OptionFactory;
+use Aura\Cli\Signal;
+use Aura\Cli\Stdio;
+use Aura\Cli\StdioResource;
+use Aura\Cli\Translator;
+use Aura\Cli\Vt100;
+use Aura\Cli\Option;
 
-$dnsResolverFactory = new React\Dns\Resolver\Factory();
-$dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
-$connector = new React\SocketClient\Connector($loop, $dns);
+class LightTableClient extends AbstractCommand {
+  private $debug;
 
-file_put_contents('log', print_r($argv, TRUE));
+  protected $options = [
+    'debug' => [
+      'long'    => 'debug',
+      'param'   => Option::PARAM_OPTIONAL,
+      'multi'   => false,
+      'default' => false,
+    ],
+  ];
 
-$port = $argv[1];
-$client_id = $argv[2];
+  public function __construct($Context, $Stdio, $Opts, $Signal, $Logger = NULL) {
+    parent::__construct($Context, $Stdio, $Opts, $Signal);
+  }
 
-$client = [
-  'name' => 'php-client',
-  'client-id' => $client_id,
-  'dir' => getcwd(),
-  'commands' => [
-    'editor.eval.php'
-  ],
-  'type' => 'php'
-];
+  protected function debug($msg) {
+    static $log;
 
-file_put_contents('log', print_r($client, TRUE), FILE_APPEND);
-$connector->create('127.0.0.1', $port)->then(function (React\Stream\Stream $stream) {
-  file_put_contents('log', 'Connected and stuff', FILE_APPEND);
-  $stream->write('Connected');
-  $stream->write(json_encode($client));
-  $stream->on('data', function($data) use ($stream) {
-    file_put_contents('log', print_r($data, TRUE), FILE_APPEND);
-  });
-  $stream->close();
-});
+    if (!$this->debug) {
+      return;
+    }
 
-/*
-$loop = React\EventLoop\Factory::create();
-$socket = new React\Socket\Server($loop);
+    if ($log === NULL) {
+      $log = new Logger('php-lt-client');
+      $log->pushHandler(new StreamHandler(__DIR__ . '/logfile.txt', Logger::DEBUG));
+    }
 
-$socket->on('connection', function ($c) {
-  $c->write("Hello LT!\n");
+    $log->addInfo($msg);
+  }
 
-  $c->on('data', function ($data) use ($c) {
-    $c->close();
-  });
-});
+  public function action() {
+    $this->debug = $this->getopt->debug;
 
-$socket->listen(1337);
-$loop->run();
- */
+    $this->debug('Running LightTableClient with parameters ' . print_r($this->params, TRUE));
+
+    $loop = React\EventLoop\Factory::create();
+
+    $dnsResolverFactory = new React\Dns\Resolver\Factory();
+    $dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
+
+    $connector = new React\SocketClient\Connector($loop, $dns);
+
+    list(, $adress, $port, $client_id) = $this->params;
+
+    $client = [
+      'name' => 'php-client',
+      'client-id' => $client_id,
+      'dir' => getcwd(),
+      'commands' => [
+        'editor.eval.php'
+      ],
+      'type' => 'php'
+    ];
+
+    $this->debug('Client: ' . print_r($client, TRUE));
+    $connector->create('127.0.0.1', $port)->then(function (React\Stream\Stream $stream) {
+      $this->debug('Connected and stuff');
+      $stream->write('Connected');
+      $stream->write(json_encode($client));
+      $stream->on('data', function($data) use ($stream) {
+        $this->debug(print_r($data, TRUE));
+      });
+      $stream->close();
+    });
+  }
+}
+
+// instantiate
+$command = new LightTableClient(
+    new Context($GLOBALS),
+    new Stdio(
+        new StdioResource('php://stdin', 'r'),
+        new StdioResource('php://stdout', 'w+'),
+        new StdioResource('php://stderr', 'w+'),
+        new Vt100
+    ),
+    new Getopt(
+        new OptionFactory,
+        new ExceptionFactory(
+            new Translator(
+                include __DIR__ . '/vendor/aura/cli/intl/en_US.php'
+            )
+        )
+    ),
+    new Signal
+);
+
+// execute
+$command->exec();
